a273a3af08838619373fb06e981da4c52904cf5f
Carlos Alberto <carlos98.camv@gmail.com>
2020-01-20 18:38:03 -0300

    Adicionada a entidade Funcionario Terceirizado(OutsourcedEmployee)

---
+++ course/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Herança_Polimosfismo.course.Entities
+{
+    class OutsourcedEmployee : Employee //A classe "OutsourcedEmployee" está herdadndo da classe "Employee"
+    {
+        public double AdditionalCharge { get; set; }
+
+        // Contrutor aproveitando todos os parametros do construtor da classe Employee por meio do 'BASE' 
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge) :base(name,hours,valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+    }
+}
